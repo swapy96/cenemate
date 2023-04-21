8651ddcc1a2a0c36304beb975da04ad484645e2b
swapy96 <s.swapnil80@gmail.com>
2023-04-20 20:13:01 -0700

    card component is completed

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -5,7 +5,7 @@ import React from "react";
 
 function AllRoutes() {
   return (
-    <>
+    <div className="dark:bg-slate-800">
       <Routes>
         <Route path="/" element={<MovieList />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
@@ -15,7 +15,7 @@ function AllRoutes() {
         <Route path="/search" element={<Search />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
