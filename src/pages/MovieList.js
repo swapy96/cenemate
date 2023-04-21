33c7878c25b10c84e1f98072cb565ee1b7152ade
swapy96 <s.swapnil80@gmail.com>
2023-04-20 18:48:11 -0700

    base stucture with header and footer is ready

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function MovieList() {
-  return (
-    <main>
-      <div className="text-9xl p-10 bg-emerald-200 border-9 border-blue-700 rounded-lg m-5">MovieList in movie</div>
-    </main>
-  );
+  return <main>{/* <div classNames="text-9xl p-10 bg-emerald-200 border-9 border-blue-700 rounded-lg m-5">MovieList in movie</div> */}</main>;
 }
 
 export default MovieList;
