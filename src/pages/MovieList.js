fd665c4a7343d692902316027d02b6bf809049a8
swapy96 <s.swapnil80@gmail.com>
2023-04-20 22:03:39 -0700

    fetched data from the tmdb api and map throught it and padded array of movies to card component

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 
 function MovieList() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=0f2592902b5162894da6f1180d0c62cf");
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    console.log(movies);
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">
-          <Card />
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
