20dda989491ffeb504c2730532e5db1bbb42f94f
swapy96 <s.swapnil80@gmail.com>
2023-04-23 17:07:38 -0700

    completes the scoll and tab title and movie details page

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Card } from "./Card";
 export { default as Button } from "./Button";
+export { ScrollToTop } from "./ScrollToTop";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-export { default as MovieDetail } from "./MovieDetail";
+export { MovieDetail } from "./MovieDetail";
 export { default as MovieList } from "./MovieList";
 export { default as PageNotFound } from "./PageNotFound";
 export { default as Search } from "./Search";
